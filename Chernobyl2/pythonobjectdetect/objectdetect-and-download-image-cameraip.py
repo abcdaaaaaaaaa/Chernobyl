3505ae6b96f336bbacd7c3c3087f486baa091c15
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2022-12-07 22:29:47 +0300

    Update objectdetect-and-download-image-cameraip.py

--- Chernobyl2/pythonobjectdetect/objectdetect-and-download-image-cameraip.py
+++ Chernobyl2/pythonobjectdetect/objectdetect-and-download-image-cameraip.py
@@ -6,3 +6,25 @@ sleep(5)
 img = pyautogui.screenshot()
 img.save(x)
 print("successfully downloaded")
+import pyautogui
+from time import sleep
+
+x = input("write your png name:")+'.png'
+sleep(5)
+img = pyautogui.screenshot()
+img.save(x)
+print("successfully downloaded")
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.1.0.h5"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , x), output_image_path=os.path.join(execution_path , x))
+
+for eachObject in detections:
+    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+
