ee34c453e871bbabd057e003c14af64cc2d507f3
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2022-12-09 15:25:49 +0300

    Update kameraobjetanima.py

--- Chernobyl2/python/kameraobjetanima.py
+++ Chernobyl2/python/kameraobjetanima.py
@@ -1,34 +1,45 @@
 import cv2
-import matplotlib.pyplot as plt
-import cvlib as cv
-import urllib.request
-import numpy as np
-from cvlib.object_detection import draw_bbox
-import concurrent.futures
- 
-url='http://192.168.1.2:81/stream'
-im=None
- 
-def run():
-    cv2.namedWindow("detection", cv2.WINDOW_AUTOSIZE)
-    while True:
-        img_resp=urllib.request.urlopen(url)
-        imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
-        im = cv2.imdecode(imgnp,-1)
- 
-        bbox, label, conf = cv.detect_common_objects(im)
-        im = draw_bbox(im, bbox, label, conf)
- 
-        cv2.imshow('detection',im)
-        key=cv2.waitKey(5)
-        if key==ord('q'):
-            break
-            
-    cv2.destroyAllWindows()
- 
- 
- 
-if __name__ == '__main__':
-    print("started")
-    with concurrent.futures.ProcessPoolExecutor() as executer:
-            f2= executer.submit(run)
+import pyautogui
+from time import sleep
+
+x = input("write your png name:")+'.png'
+sleep(5)
+img = pyautogui.screenshot()
+img.save(x)
+print("successfully downloaded")
+image = cv2.imread(x)
+image = cv2.resize(image, (1500, 1000))
+h = image.shape[0]
+w = image.shape[1]
+
+weights = "ssd_mobilenet/frozen_inference_graph.pb"
+model = "ssd_mobilenet/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+net = cv2.dnn.readNetFromTensorflow(weights, model)
+
+class_names = []
+with open("ssd_mobilenet/coco_names.txt", "r") as f:
+    class_names = f.read().strip().split("\n")
+
+blob = cv2.dnn.blobFromImage(
+    image, 1.0/127.5, (320, 320), [127.5, 127.5, 127.5])
+net.setInput(blob)
+output = net.forward()  
+
+for detection in output[0, 0, :, :]:  
+    probability = detection[2]
+
+   
+    if probability < 0.5:
+        continue
+   
+    box = [int(a * b) for a, b in zip(detection[3:7], [w, h, w, h])]
+    box = tuple(box)
+    cv2.rectangle(image, box[:2], box[2:], (0, 255, 0), thickness=2)
+
+    class_id = int(detection[1])
+    label = f"{class_names[class_id - 1].upper()} {probability * 100:.2f}%"
+    cv2.putText(image, label, (box[0], box[1] + 15),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+cv2.imshow('Image', image)
+cv2.waitKey()
